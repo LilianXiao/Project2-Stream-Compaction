9d687b01de420e182943cc8c928ca8e711b1fe3c
Lilian Xiao <lilianxia456@gmail.com>
2026-09-19 00:28:13 -0400

    Parts 0 + 1

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <vector>
 #include "cpu.h"
 
 #include "common.h"
@@ -12,6 +13,39 @@ namespace StreamCompaction {
             return timer;
         }
 
+        // CPU versions of kernMapToBoolean and kernScatter, and the scan method
+        namespace {
+            void scanner(int n, int* odata, const int* idata) {
+                int sum = 0;
+                for (int i = 0; i < n; ++i) {
+                    // avoid accidentally overwriting idata
+                    int temp = idata[i];
+                    odata[i] = sum;
+                    sum += temp;
+                }
+            }
+
+            void mapToBoolean(int n, int* bools, const int* idata) {
+                for (int i = 0; i < n; ++i) {
+                    if (idata[i] != 0) {
+                        bools[i] = 1;
+                    }
+                    else {
+                        bools[i] = 0;
+                    }
+                }
+            }
+
+            void scatter(int n, int* odata, const int* idata,
+                const int* bools, const int* indices) {
+                for (int i = 0; i < n; ++i) {
+                    if (bools[i] == 1) {
+                        odata[indices[i]] == idata[i];
+                    }
+                }
+            }
+        }
+
         /**
          * CPU scan (prefix sum).
          * For performance analysis, this is supposed to be a simple for loop.
@@ -19,7 +53,9 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+
+            scanner(n, odata, idata);
+
             timer().endCpuTimer();
         }
 
@@ -30,9 +66,16 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+
+            int num = 0;
+            for (int i = 0; i < n; ++i) {
+                if (idata[i] != 0) {
+                    odata[num++] = idata[i];
+                }
+            }
+
             timer().endCpuTimer();
-            return -1;
+            return num;
         }
 
         /**
@@ -41,10 +84,24 @@ namespace StreamCompaction {
          * @returns the number of elements remaining after compaction.
          */
         int compactWithScan(int n, int *odata, const int *idata) {
+            if (n <= 0) {
+                return 0;
+            }
+            std::vector<int> bools(n);
+            std::vector<int> indices(n);
+
             timer().startCpuTimer();
-            // TODO
+
+            mapToBoolean(n, bools.data(), idata);
+
+            scanner(n, indices.data(), bools.data());
+
+            scatter(n, odata, idata, bools.data(), indices.data());
+
+            int num = indices[n - 1] + bools[n - 1];
+
             timer().endCpuTimer();
-            return -1;
+            return num;
         }
     }
 }
